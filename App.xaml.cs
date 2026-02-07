d047c385d1c218c768c6a1539bfdbbc38d12e55d
robert123bacalso@gmail.com <robert123bacalso@gmail.com>
2026-02-06 17:27:14 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Tasker.MVVM.Views;
+
+namespace Tasker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainView());
+        }
+
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new NavigationPage(new MainPage()));
+        //}
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
